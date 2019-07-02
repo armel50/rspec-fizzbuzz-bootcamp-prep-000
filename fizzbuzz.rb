d88ee0a1cb3e550ba230e13deaf0bbbff6397165
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-02 22:08:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,9 +6,12 @@ def fizzbuzz(num)
   if num % 3 = 0
     return "Fizz"
     
-    elsif num % 5
+    elsif num % 5=0
       return "Buzz"
-      
+      elsif num % 3=0 && num % 5=0
+      return "FizzBuzz"
+    else 
+      return nil
   end
 
   
